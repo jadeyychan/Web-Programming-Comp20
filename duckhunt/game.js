3e04b984875fbfc35d336c8b9728607d5a316b62
Jade <jadeyychan@gmail.com>
2015-10-11 15:11:50 -0400

    Display background and 2 static birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,22 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext('2d');
+
+	var background = new Image();
+	var birds = new Image();
+
+	background.onload = function () {
+		/* Background */
+		ctx.drawImage(background, 0,0, 256, 240, 0, 0, 640, 600);
+
+		/* Bird #1 */
+		ctx.drawImage(birds, 127, 121, 40, 30, 200, 150, 80, 60);
+
+		/* Bird #2 */
+		ctx.drawImage(birds, 335, 115, 40, 40, 350, 100, 80, 80);
+	}
+
+	/* Linked images */
+	background.src = "duckhunt-background.gif";
+	birds.src = "duckhunt_various_sheet.png";
+}
