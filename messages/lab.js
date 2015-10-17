3b59556b811e568aaa9fbc3f2d836d3c582a5ce5
Jade <jadeyychan@gmail.com>
2015-10-17 17:26:54 -0400

    added debuggin statements

--- messages/lab.js
+++ messages/lab.js
@@ -6,9 +6,14 @@ function parse() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			  	var response = xhr.responseText;
 			  	parsed_objects = JSON.parse(response);
+			  	console.log("about to print response");
+			  	console.log(response);
+			  	console.log("about to print parsed_objects");
+			  	console.log(parsed_objects);
 	    		elem = document.getElementById("messages");
 	    		
 	    		for (i = 0; i < parsed_objects.length; i++) {
+		    		console.log("made it into parsed objects for loop!");
 		    		var content = parsed_objects[i]["content"];
 		    		var username = parsed_objects[i]["username"];
 		            elem.innerHTML += "<p>" + "<c>" + content + "</c>" + "<us>" + username + "</us>"+ "</p>";
