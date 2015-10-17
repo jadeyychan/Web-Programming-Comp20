0e1698b2e8b98f1a826e72970a0eae1c20445add
Jade <jadeyychan@gmail.com>
2015-10-17 14:32:00 -0400

    changed count to i

--- messages/lab.js
+++ messages/lab.js
@@ -3,7 +3,7 @@ function parse() {
     parsed_objects = JSON.parse(jsondata);
     elem = document.getElementById("messages");
     for (i = 0; i < parsed_objects.length; i++) {
-            console.log(Object.keys(parsed_objects[count]));
+            console.log(Object.keys(parsed_objects[i]));
             elem.innerHTML += "<p> ITEM 1 " + parsed_objects[i]["content"] + parsed_objects[i]["username"] + "</p>";
     }
 }
