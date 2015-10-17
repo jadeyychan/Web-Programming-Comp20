f30b1c69ecd2711f7c742546230d2185200ba45a
Jade <jadeyychan@gmail.com>
2015-10-17 13:45:42 -0400

    changed id name to match html

--- messages/lab.js
+++ messages/lab.js
@@ -1,7 +1,7 @@
 function parse_data() {
     jsondata = '[{"id":1,"content":"Do you really want to hurt me?","username":"Boy"},{"id":2,"content":"Do you really want to make me cry?","username":"George"}]'
     parsed_objects = JSON.parse(jsondata);
-    elem = document.getElementById("results");
+    elem = document.getElementById("messages");
     for (i = 0; i < parsed_objects.length; i++) {
             elem.innerHTML += "<p> ITEM 1 " + parsed_objects[i]["content"] + parsed_objects[i]["username"] + </p>;
     }
