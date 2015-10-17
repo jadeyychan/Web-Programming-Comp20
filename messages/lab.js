83d339bfe62427581b2f87315185a18114f90e1a
Jade <jadeyychan@gmail.com>
2015-10-17 13:41:28 -0400

    added json code from class example

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,9 @@
-// Your JavaScript goes here...
+function parse_data() {
+    jsondata = "data.json";
+    parsed_objects = JSON.parse(jsondata);
+    elem = document.getElementById("results");
+    for (i = 0; i < parsed_objects.length; i++) {
+            elem.innerHTML += "<p> ITEM 1 " + parsed_objects[i]["content"] + parsed_objects[i]["username"] + </p>;
+    }
+}
+
