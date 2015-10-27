21c9ce9acc900699612294c3842e91b25c511138
Jade <jadeyychan@gmail.com>
2015-10-27 16:11:17 -0400

    got input field and prepends underneathe

---
+++ captainslog/captainslog.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	$("input").on("change", function() {
+		var message = ($(this).val());
+		$("#log").prepend("<div>"+message+"</div>");
+	});
+});	
+
+
+
+	// var input = $("input").val();
+	// console.log("input is");
+	// console.log(input);
+
+	// $("msg").change(function() {
+	// 	console.log("do we get here?", message);
+	// 	var message = $(".msg").text();
+	// 	console.log("message is", message);
+	// // $(".log").prepend(message);
+	// });
+
+
+// $("#myTextBox").on("change paste keyup", function() {
+//    alert($(this).val()); 
+// });
