3bb68c42c00792815b902b68774a21a1e5fed1de
Jade <jadeyychan@gmail.com>
2015-10-28 19:32:01 -0400

    added timestamp

--- captainslog/captainslog.js
+++ captainslog/captainslog.js
@@ -2,25 +2,10 @@ $(document).ready(function() {
 
 	$("input").on("change", function() {
 		var message = ($(this).val());
-		$("#log").prepend("<div>"+message+"</div>");
+		var timestamp = new Date();
+		var entry = timestamp+" - "+message;
+
+		$("#log").prepend("<div>"+entry+"</div>");
 		$(this).val(""); // resetting the input field to empty
 	});
 });	
-
-
-
-	// var input = $("input").val();
-	// console.log("input is");
-	// console.log(input);
-
-	// $("msg").change(function() {
-	// 	console.log("do we get here?", message);
-	// 	var message = $(".msg").text();
-	// 	console.log("message is", message);
-	// // $(".log").prepend(message);
-	// });
-
-
-// $("#myTextBox").on("change paste keyup", function() {
-//    alert($(this).val()); 
-// });
